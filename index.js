c365f14a9495ad9b406e5291fb10ab062ea60b17
jonss0777 <jonss0777@gmail.com>
2024-07-12 13:40:44 -0400

    adjusting slideshow

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let sliderImages = document.querySelectorAll(".slide"),
+	arrowLeft = document.querySelector("#arrow-left"),
+	arrowRight = document.querySelector("#arrow-right"),
+	current = 0;
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// Clear all images
+function reset() {
+	for (let i = 0; i < sliderImages.length; i++) {
+		sliderImages[i].style.display = "none";
+	}
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+// Initial slide
+function startSlide() {
+	reset();
+	sliderImages[0].style.display = "block";
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
+// Show previous
+function slideLeft() {
+	reset();
+	sliderImages[current - 1].style.display = "block";
+	current--;
+}
+
+// Show next
+function slideRight() {
+	reset();
+	sliderImages[current + 1].style.display = "block";
+	current++;
+}
+
+// Left arrow click
+arrowLeft.addEventListener("click", function () {
+	if (current === 0) {
+		current = sliderImages.length;
+	}
+	slideLeft();
+});
+
+// Right arrow click
+arrowRight.addEventListener("click", function () {
+	if (current === sliderImages.length - 1) {
+		current = -1;
+	}
+	slideRight();
+});
+
+startSlide();
